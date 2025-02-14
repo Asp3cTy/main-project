9d377a74fc217b634bb8e8d8a69b76df72d7d1d2
Julio Moriera <julinhopentakill@gmail.com>
2025-02-14 19:31:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ require('dotenv').config(); // Apenas para ambiente local
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: "database-2.cfs2cmiaucif.sa-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "palmares",
+    database: "app_db",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
