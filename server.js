292932d200c8a8d461139757357bbc0a190ff54b
Julio Moriera <julinhopentakill@gmail.com>
2025-02-14 13:24:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ const cors = require("cors");
 const bodyParser = require("body-parser"); // ou express.json() nativo
 const path = require('path');
 
-// Importa a pool de conexão
-const db = require("./db");
+require('dotenv').config(); // Apenas para ambiente local
+
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,  // Ajuste conforme necessário
+    queueLimit: 0
+});
+
+module.exports = pool.promise(); // Se estiver usando async/await
 
 const app = express();
 
