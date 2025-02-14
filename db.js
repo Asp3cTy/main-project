514c27c510a378a9d1f77226f91aa4430c630825
Julio Moriera <julinhopentakill@gmail.com>
2025-02-14 01:26:28 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 // db.js
+require('dotenv').config(); // Carrega as variáveis do .env
 const mysql = require("mysql2");
 
 // Crie a pool de conexão (ou use createConnection se preferir)
 const pool = mysql.createPool({
-    host: "database-2.cfs2cmiaucif.sa-east-1.rds.amazonaws.com",
-    user: "admin",
-    password: "palmares",
-    database: "app_db",
-
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 // Exporta a pool para usar em outros módulos
 module.exports = pool;
+
+
